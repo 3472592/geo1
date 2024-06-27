e987ce3af68a988996baaf09d04028240c0f4e6a
3472592 <3472592@gmail.com>
2024-06-27 06:58:19 -0400

    Add files via upload

---
+++ distance/distance.js
@@ -0,0 +1,70 @@
+/* myLoc.js */
+
+var ourCoords =  {
+	latitude: 39.952583,
+	longitude: -75.165222
+};
+
+window.onload = getMyLocation;
+
+function getMyLocation() {
+	if (navigator.geolocation) {
+
+		navigator.geolocation.getCurrentPosition(
+			displayLocation, 
+			displayError);
+	}
+	else {
+		alert("Oops, no geolocation support");
+	}
+}
+
+function displayLocation(position) {
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+
+	var div = document.getElementById("location");
+	div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+
+	var km = computeDistance(position.coords, ourCoords);
+	var distance = document.getElementById("distance");
+	distance.innerHTML = "You are " + km + " km away from Philly U.S.";
+
+}
+// passing the coors of my position and coors to compute distance
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+
+	var Radius = 6371; // radius of the Earth in km
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + 
+					Math.cos(startLatRads) * Math.cos(destLatRads) *
+					Math.cos(startLongRads - destLongRads)) * Radius;
+
+	return distance;
+}
+
+function degreesToRadians(degrees) {
+	radians = (degrees * Math.PI) / 180;
+	return radians;
+}
+
+// ------------------ End Ready Bake -----------------
+
+
+function displayError(error) {
+	var errorTypes = {
+		0: "Unknown error",
+		1: "Permission denied",
+		2: "Position is not available",
+		3: "Request timeout"
+	};
+	var errorMessage = errorTypes[error.code];
+	if (error.code == 0 || error.code == 2) {
+		errorMessage = errorMessage + " " + error.message;
+	}
+	var div = document.getElementById("location");
+	div.innerHTML = errorMessage;
+}
